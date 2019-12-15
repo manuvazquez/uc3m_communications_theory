07cb7ec84d710205c21bbd5a844187caa29e9889
Manuel A. Vázquez <manuavazquez@gmail.com>
2019-12-15 20:45:07 +0100

    - teaching     - *.ipynb files moved to "notebooks" directory     - code for sampling refactored into "sampling.py" module     - added "law of large numbers.ipynb"

---
+++ teaching/notebooks/sampling.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.stats
+
+
+def gaussian_samples(n: int, mean: float = 1., sd: float = 1.) -> np.ndarray:
+
+	return scipy.stats.norm(mean, sd).rvs(n)
+
+
+def uniform_samples(n: int, left: float = 0., right: float = 2.) -> np.ndarray:
+
+	return scipy.stats.uniform(left, right).rvs(n)
+
+
+def poisson_samples(n: int, mu: float = 2) -> np.ndarray:
+
+	return scipy.stats.poisson(mu).rvs(n)
