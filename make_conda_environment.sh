7adf288888fbd3e493aaa8ba9766448013df5492
Manuel A. Vázquez <manuavazquez@gmail.com>
2021-12-29 20:17:43 +0100

    - labs 	- notebooks 		- plotly_analog_modulations_lab.ipynb 			- minor improvements - make_conda_environment.sh 	- updated

--- make_conda_environment.sh
+++ make_conda_environment.sh
@@ -6,4 +6,4 @@ MANAGER="mamba"
 # only required if "anaconda" is not in the path
 source $HOME/anaconda3/etc/profile.d/conda.sh
 
-$MANAGER create --yes -n $NAME python=3 ipdb jupyterlab ipywidgets numpy matplotlib scipy voila librosa fastcore ipympl -c defaults -c conda-forge -c fastai
+$MANAGER create --yes -n $NAME python=3 ipdb jupyterlab ipywidgets numpy matplotlib scipy voila librosa fastcore ipympl plotly pandas -c defaults -c conda-forge -c fastai
