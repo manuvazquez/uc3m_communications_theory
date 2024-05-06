3508498ed5eea8da772e279a36b981f568fcc189
Manuel A. Vázquez <manuavazquez@gmail.com>
2024-05-06 12:51:19 +0200

    - make-conda-environment.sh 	- new format - added "voila.json"

--- make-conda-environment.sh
+++ make-conda-environment.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-NAME=ct
 MANAGER="mamba"
 
-# only required if "anaconda" is not in the path
-source $HOME/anaconda3/etc/profile.d/conda.sh
+NAME=ct
 
-$MANAGER create --yes -n $NAME python=3 ipdb jupyterlab ipywidgets numpy matplotlib scipy voila librosa fastcore ipympl plotly pandas -c defaults -c conda-forge -c fastai
+LIBRARIES=(
+    python
+    ipdb
+    jupyterlab ipywidgets
+    numpy
+    matplotlib
+    scipy
+    voila
+    librosa
+    fastcore
+    ipympl
+    plotly
+    pandas
+)
+
+CHANNELS=(
+    defaults
+    conda-forge
+    fastai
+)
+
+# ---
+
+COLOR="\033[40m\033[32m"
+UNCOLOR="\033[0m"
+
+
+# ------------ setup
+
+# only required if "anaconda" is not in the path
+source $HOME/$MY_CONDA_INSTALLATION/etc/profile.d/conda.sh
+
+# from https://stackoverflow.com/a/9429887/3967334
+LIBRARIES_CONCATENATED=$(IFS=" " ; echo "${LIBRARIES[*]}")
+
+# from https://stackoverflow.com/a/17841619/3967334
+function join_by { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
+
+CHANNELS_CONCATENATED=$(join_by ' -c ' "${CHANNELS[@]}")
+
+# ------------ installation
+
+$MANAGER create --yes -n $NAME $LIBRARIES_CONCATENATED -c $CHANNELS_CONCATENATED
+
+# ------------ pip
+
+conda activate $NAME
+
+# pip stuff here....
+
+# ------------
+
+echo -e new environment is \"$COLOR$NAME$UNCOLOR\"
